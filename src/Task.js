5c443297a1867738e9911c279184ee70b498fe62
al akhyaar <akhyaarelm@gmail.com>
2023-06-19 10:29:47 +0700

    first commit

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "M. Mushthofa Al-Akhyar" // TODO: replace this
+const studentId = "FE5555646" // TODO: replace this
+
+export { url, studentName, studentId }
