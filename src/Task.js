3162371cec45e78e479b7a111f6af2d1df18fc1a
al akhyaar <akhyaarelm@gmail.com>
2023-06-19 10:34:02 +0700

    2nd commit

--- src/Task.js
+++ src/Task.js
@@ -1,4 +1,4 @@
-const url = "YOURDEPLOYURL" // TODO: replace this
+const url = "https://sprightly-maamoul-c66417.netlify.app" // TODO: replace this
 const studentName = "M. Mushthofa Al-Akhyar" // TODO: replace this
 const studentId = "FE5555646" // TODO: replace this
 
